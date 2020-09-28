5cb3f97f3e43ac43224b7064623d07cf718444d7
Michael Gamble <mick_g@htroll.com>
2020-09-28 08:42:25 -0700

    Right after setup and adding the models folder.

---
+++ DoToo/Models/TodoItem.cs
@@ -0,0 +1,28 @@
+﻿// ***********************************************************************
+// Assembly         : DoToo
+// Author           : mick_
+// Created          : 09-28-2020
+//
+// Last Modified By : mick_
+// Last Modified On : 09-28-2020
+// ***********************************************************************
+// <copyright file="TodoItem.cs" Application="DoToo">
+//     Copyright (c) Happy Troll Computing. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace DoToo.Models
+{
+    using System;
+
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool Completed { get; set;  }
+
+        public DateTime Due { get; set; }
+    }
+}
